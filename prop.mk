93e800e3cffb322ab63d3e553d79fc2f3924b5f0
mdeejay <mdjrussia@gmail.com>
2019-01-04 13:07:12 +0100

    sdm450-common: Enable QCOM Boost framework
    * Thanks to @kondors1995 for idea

--- prop.mk
+++ prop.mk
@@ -142,6 +142,15 @@ vendor.video.disable.ubwc=1
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.vendor.extension_library=libqti-perfd-client.so
 
+#QTI Performance
+PRODUCT_PROPERTY_OVERRIDES += \
+vendor.enable_prefetch=1 \
+vendor.iop.enable_uxe=1 \
+vendor.iop.enable_prefetch_ofr=1 \
+vendor.perf.iop_v3.enable=1 \
+ro.vendor.at_library=libqti-at.so \
+persist.vendor.qti.games.gt.prof=1
+
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.use_data_netmgrd=true \
